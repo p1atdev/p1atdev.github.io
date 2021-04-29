2f761590772684d4eebced1eb7033fd8e5f5f672
Shu Teiei <shu_teiei@outlook.jp>
2021-04-29 14:52:37 +0900

    fix: delete duplicate classes

--- pages/apps.vue
+++ pages/apps.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="container mx-auto px-4 md:container md:mx-auto">
 		<title>Plationa - Apps</title>
-		<h1 class="my-8 my-8 text-center self-center text-8xl dark:text-white">
+		<h1 class="my-8 text-center self-center text-8xl dark:text-white">
 			Comig soon...
 		</h1>
 	</div>
